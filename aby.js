fca420fa5fa4bd5df57c9f8e25a2fed9ed41ac8e
JANBRAYAN\janbr <janbrayanbecasalario@gmail.com>
2024-12-09 20:19:54 -0600

    Agregado endpoint de status

--- aby.js
+++ aby.js
@@ -6,24 +6,25 @@ const app = express();
 // Ruta para skipear la canción
 app.get('/skipsong', async (req, res) => {
   try {
-    // Llama a la API de Nightbot para skipear la canción
     const response = await axios.post(
       'https://api.nightbot.tv/1/song_requests/queue/skip',
-      {}, // Sin body
+      {},
       {
         headers: {
-          Authorization: `Bearer ${process.env.ACCESS_TOKEN}`, // Usa la variable de entorno para el token
+          Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
         },
       }
     );
-
-    // Respuesta en caso de éxito
     res.status(200).send('¡La canción ha sido saltada con éxito!');
   } catch (error) {
-    // Manejo de errores
     console.error('Error al intentar skipear la canción:', error.response?.data || error.message);
     res.status(500).send('Hubo un problema al intentar skipear la canción.');
   }
+});
+
+// Ruta para verificar el estado de la API
+app.get('/status', (req, res) => {
+  res.status(200).send('API funcionando');
 });
 
 // Iniciar el servidor
